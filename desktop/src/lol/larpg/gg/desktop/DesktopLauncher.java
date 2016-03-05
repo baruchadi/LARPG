d56549d6f6c90d9a3ac06ec053230ef2865b06fe
baruch hen <baruchadi@gmail.com>
2016-03-05 00:57:44 -0500

    initial setup
    initial setup

---
+++ desktop/src/lol/larpg/gg/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package lol.larpg.gg.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import lol.larpg.gg.MyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGame(), config);
+	}
+}
